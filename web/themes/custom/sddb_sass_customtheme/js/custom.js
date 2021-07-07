cde2a5faac7f94337170bc47efcbc91f8cf3f52a
allahnoor turab <turabdev@gmail.com>
2021-07-07 17:42:17 +0500

    Front page update

--- web/themes/custom/sddb_sass_customtheme/js/custom.js
+++ web/themes/custom/sddb_sass_customtheme/js/custom.js
@@ -13,29 +13,6 @@
       // Custom code here
 
 
-      // Back to top button
-      $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-          $('.back-to-top').fadeIn('slow');
-        } else {
-          $('.back-to-top').fadeOut('slow');
-        }
-      });
-
-      $('.back-to-top').click(function () {
-        $('html, body').animate({
-          scrollTop: 0
-        }, 1500, 'easeInOutExpo');
-        return false;
-      });
-
-      // venobox
-      $(document).ready(function () {
-        if ($(".venobox")[0]) {
-
-          $('.venobox').venobox();
-        }
-      });
 
     }
   };
